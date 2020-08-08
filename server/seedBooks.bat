e957004af2e9d1bda946baf89fb0a6c78d24e046
AliAhmedNada <aliahmedalihassan@gmail.com>
2020-08-08 16:31:37 +0200

    first commit

--- server/seedBooks.bat
+++ server/seedBooks.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Seeding Books..
-mongoimport --db BookStore --collection books --file books.json
+mongoimport --db BookStore --collection books --file books11.json --jsonArray
 :finish
 pause
